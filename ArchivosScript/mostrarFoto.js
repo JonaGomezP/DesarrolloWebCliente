e697e1cb561b86a7a8c2d49b38afdc6068a28b3b
Jonatan Gomez <jgp0007@alu.medac.es>
2021-10-22 12:13:08 +0200

    ejercicio 3 y 4 e imágenes

---
+++ ArchivosScript/mostrarFoto.js
@@ -0,0 +1,41 @@
+function fotoAnterior() {
+
+    let fotoMostrada = document.querySelector("img");
+    let atributoSRC = fotoMostrada.getAttribute("src");
+    switch (atributoSRC) {
+        case "../img/5.jpg":
+            fotoMostrada.setAttribute("src", "../img/4.jpg");
+            break;
+        case "../img/4.jpg":
+            fotoMostrada.setAttribute("src", "../img/3.jpg");
+            break;
+        case "../img/3.jpg":
+            fotoMostrada.setAttribute("src", "../img/2.jpg");
+            break;
+        case "../img/2.jpg":
+            fotoMostrada.setAttribute("src", "../img/1.jpg");
+            break;
+    }
+}
+
+function fotoSiguiente() {
+
+    let fotoMostrada = document.querySelector("img");
+    let atributoSRC = fotoMostrada.getAttribute("src");
+    switch (atributoSRC) {
+        case "../img/1.jpg":
+            fotoMostrada.setAttribute("src", "../img/2.jpg");
+            break;
+        case "../img/2.jpg":
+            fotoMostrada.setAttribute("src", "../img/3.jpg");
+            break;
+        case "../img/3.jpg":
+            fotoMostrada.setAttribute("src", "../img/4.jpg");
+            break;
+        case "../img/4.jpg":
+            fotoMostrada.setAttribute("src", "../img/5.jpg");
+            break;
+    }
+
+
+}
