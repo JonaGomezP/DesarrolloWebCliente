bdfb6696e5363ba3c9ae90eafc8da76d2400bc9c
JonaGomezP <jgp0007@alu.medac.es>
2021-11-20 16:24:31 +0100

    bombillas actualizadas

--- JS/bombillas.js
+++ JS/bombillas.js
@@ -1,20 +1,59 @@
-window.onload = function(){
+window.onload = function () {
     const bombillas = document.getElementsByTagName("input");
     let id = 0;
     for (const key in bombillas) {
         bombillas[key].id = id;
         bombillas[key].className = "apagada";
-        bombillas[key].onclick = function (){
+        bombillas[key].onclick = function () {
             presionar(bombillas[key].getAttribute("id"));
         }
-        id ++;
+        id++;
     }
 }
 
-function presionar(idBombilla){
+function presionar(idBombilla) {
+    let bombillasTotales = document.getElementsByTagName("input");
     let bombillaActual = document.getElementById(idBombilla);
     let estado = bombillaActual.getAttribute("class");
-    if(estado == "apagada"){
+    if(bombillaActual.getAttribute("cass") == "apagada"){
         bombillaActual.className = "encendida";
+    } else{
+        bombillaActual.className = "apagada";
+    }
+    idBombilla = parseInt(idBombilla);
+    if (idBombilla > 0 && idBombilla < bombillasTotales.length - 1) {
+        if (bombillasTotales[idBombilla - 1].getAttribute("class") == "apagada") {
+            bombillasTotales[idBombilla - 1].className = "encendida";
+        } else {
+            bombillasTotales[idBombilla - 1].className = "apagada";
+        }
+        if (bombillasTotales[idBombilla + 1].getAttribute("class") == "apagada") {
+            bombillasTotales[idBombilla + 1].className = "encendida";
+        } else {
+            bombillasTotales[idBombilla + 1].className = "apagada";
+        }
+    } else if (idBombilla == 0) {
+        if (bombillasTotales[idBombilla + 1].getAttribute("class") == "apagada") {
+            bombillasTotales[idBombilla + 1].className = "encendida";
+        } else {
+            bombillasTotales[idBombilla + 1].className = "apagada";
+        }
+    } else {
+        if (bombillasTotales[idBombilla - 1].getAttribute("class") == "apagada") {
+            bombillasTotales[idBombilla - 1].className = "encendida";
+        } else {
+            bombillasTotales[idBombilla - 1].className = "apagada";
+        }
+    }
+
+    let contador = 0;
+    for (const elemento of bombillasTotales) {
+        if (elemento.getAttribute("class") == "encendida") {
+            contador++;
+            if (contador == bombillasTotales.length) {
+                alert("!!!Has ganado¡¡¡");
+            }
+        }
+        
     }
 }
