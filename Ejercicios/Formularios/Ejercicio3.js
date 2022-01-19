80e4b4815f04aa2f70cba00ba893b660c954648a
Jonatan Gomez <jgp0007@alu.medac.es>
2022-01-19 12:02:58 +0100

    ejercicio 3 terminado

--- Ejercicios/Formularios/Ejercicio3.js
+++ Ejercicios/Formularios/Ejercicio3.js
@@ -2,18 +2,16 @@ window.onload = function(){
     let boton = document.querySelector("input[type='submit']");
     boton.onclick = function(e){
         e.preventDefault();
-        console.log("hola");
         escribir();
     }
 }
 
 function escribir(){
-    let formu = document.forms;
-    let area = formu.area;
-    let listaCheck = document.querySelectorAll("input");
+    let area = document.querySelector("textarea");
+    let listaCheck = document.querySelectorAll("input[type='checkbox'");
     listaCheck.forEach(element => {
         if(element.checked){
-            listaCheck.area;
+            area.value += element.value + " ";
         }
     });
     
