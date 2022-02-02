81a287208fcdd5936001e6f4fa6785b69165f4f1
Jonatan Gomez <jgp0007@alu.medac.es>
2022-02-02 14:41:40 +0100

    ejemplo ajax nuevo

---
+++ Ejercicios/Ajax/ajax2.js
@@ -0,0 +1,37 @@
+window.onload = function(){
+
+    document.querySelector("input[type='button']").onclick = function(){
+        this.disabled = true;
+
+        //Paso 1: crear el objeto XMLHttpRequest
+        let manolito; //variable que va a almacenar el objeto
+        if(window.XMLHttpRequest){
+            manolito = new XMLHttpRequest();
+        }
+        else{
+            manolito = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+
+        //Paso 2: crear la peticion de datos
+        //Parámetros: el verbo, la direccion del recurso
+        manolito.open('GET',"https://jsonplaceholder.typicode.com/users");
+
+        //Paso 3: crear la funcion para manejar los datos que me llegan (se asocia al evento LOAD)
+        manolito.addEventListener('load',function(datos){
+            let respuesta = JSON.parse(datos.target.response);
+
+           // document.getElementById("parra").innerHTML = respuesta;
+            document.querySelector("input[type='button']").disabled = false;
+            console.log(respuesta);
+            
+        });
+
+        //Paso 4: enviar la peticion
+        manolito.send();
+
+        console.log("SOY EL ULTIMO");
+    }
+
+
+
+}
